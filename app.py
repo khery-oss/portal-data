645c53a96ec27b1adf9ce9cf6e8ca47a4fea76eb
khery-oss <khery588@gmail.com>
2026-09-04 01:12:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.write("""
 Selamat datang di repositori eksplorasi data Indonesia, khususnya ekonomi, pembangunan, dan finansial.
 Pilih sumber data yang ingin diakses melalui menu navigasi di bilah sisi (**sidebar**) sebelah kiri:
 
-* **🏛️ World Bank:** Data indikator pembangunan lintas sektor (makro, sosial, kemiskinan, lingkungan).
+* **🌐 World Bank:** Data indikator pembangunan lintas sektor (makro, sosial, kemiskinan, lingkungan).
 * **📈 FRED:** Data makro-finansial frekuensi tinggi dari Federal Reserve Bank of St. Louis.
 * **📊 BPS & Satu Data:** *(Segera hadir)*
 """)
